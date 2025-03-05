439889fcadbda108bee20e35a801c7ed9bcfaadf
aschauhan <43375976+aschauhan@users.noreply.github.com>
2025-03-05 19:29:07 +0530

    Update cluster_policy.tf

--- cluster_policy.tf
+++ cluster_policy.tf
@@ -1,7 +1,7 @@
 access_entries = {
       devansh = {
         kubernetes_group = []
-        principal_arn     = "arn:aws:iam::32634537952:user/anuj.s@gmail.com"
+        principal_arn     = "arn:aws:iam::32634537952:user/anuj.s@gmail.com"  <-- give the correct ARN details
 
         policy_associations = {
           admin = {
